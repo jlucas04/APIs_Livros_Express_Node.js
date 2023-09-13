c966e10a9cc6cb712cbb78aaf195546e830d0c86
João Ribeiro <jlucas_04@hotmail.com>
2023-09-13 09:05:50 -0300

    configurado package.json e criado app.js com alguns métodos e funções

---
+++ server.js
@@ -0,0 +1,14 @@
+import { Console } from "console";
+//import http from "http";
+import app from "./src/app.js"
+const PORT = 3000;
+
+//const server = http.createServer((req, res) => {
+//    res.writeHead(200, {"content-type" : "text/plain"});
+ //   res.end(ROTAS[req.url]);
+//
+//});
+
+app.listen(PORT, () => {
+    console.log("Servidor escutando!");
+});
